cd8af07a31542250b10107bfe1c3d3cae852f723
Rémi Piérot <rfpierot91@gmail.com>
2015-01-06 08:59:56 +0100

    dézippage dossier Eclipse

---
+++ AeroTASAPIHA - Eclipse/Aero-TASAPIHA/src/aeroport/GestionQuai.java
@@ -0,0 +1,8 @@
+package aeroport;
+
+public interface GestionQuai {
+    
+    public abstract void atterrissage(Avion avion);
+    public abstract void decollage();
+
+}
---
+++ AeroTASAPIHA - Eclipse/Aero-TASAPIHA/src/aeroport/GestionnaireAero.java
@@ -0,0 +1,15 @@
+package aeroport;
+
+public class GestionnaireAero {
+    
+    private Aeroport aeroport;
+
+    public Aeroport getAeroport() {      
+        return this.aeroport;
+    }
+
+    public void setAeroport(Aeroport aeroport) {
+        this.aeroport = aeroport;
+    }
+
+}
